1f8219c9b8edc7f9a65584dc1998315f83c070e0
calyx-r3ppl328 <b2029cpcpatron@pshs.edu.ph>
2025-08-07 10:22:58 +0800

    boba

--- public/script.js
+++ public/script.js
@@ -10,5 +10,5 @@ numPerDay = 1;
 let totalRequired;
 totalRequired = (numPerDay * 365) * (maxAge - age);
 
-let result = "You will need " + totalRequired + " cups of milk tea to last you until the ripe old age of " + maxAge + ".";
+let result = "You will need " + totalRequired + " cups of boba tea to last you until the ripe old age of " + maxAge + ".";
 document.write(result)
