6db066ebd13ab8b0f501a32068f68c9e3c88c310
calyx-r3ppl328 <b2029cpcpatron@pshs.edu.ph>
2025-08-07 10:14:09 +0800

    nongraded

---
+++ public/script.js
@@ -0,0 +1,17 @@
+let age;
+age = 14;
+
+let maxAge;
+maxAge = 8723;
+
+let numPerDay;
+numPerDay = 1;
+
+let totalRequired;
+totalRequired = (numPerDay * 365) * (maxAge - age);
+
+<body>
+    <p> 
+        "You will need " + totalRequired + " cups of milk tea to last you until the ripe old age of " + maxAge + "."
+    </p>
+</body>
